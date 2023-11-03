7b9fca20444584bd2013e0f17f3e3db6812f4c4e
Kosoobin <143787961+Kosoobin@users.noreply.github.com>
2023-11-03 17:46:53 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,36 @@
+""" my_set = {5,8,3,7,1,"h"}
+print(my_set)
+#my_set.clear()
+my_set.remove(2)
+#my_set.discard(5)
+print(my_set) """
+
+""" my_int = 10
+my_str = str(my_int)
+print (my_int)
+print (my_str) """
+
+""" my_int = 10
+print (my_int)
+print (my_int + 10)
+my_str = str(my_int)
+print(my_str)
+print(my_str + 10)
+print(my_str + " hello") """
+
+""" my_str = '10'
+print (my_str)
+my_int = int(my_str)
+print (my_int)
+#print (my_int + 10)
+my_int2 = int("ten")
+print(my_int2) """
+
+for i in range(1,6):
+ space = " "*(6-i)
+ star = "*"*(2*i-1)
+ print(space+star)
+for i in range (6,0,-1):
+    space = " "*(6-i)
+    star = "*"*(2*i-1)
+    print (space +star)
